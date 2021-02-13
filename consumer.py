148654ba076f31f2aaddd1049e779873ebf21f2d
TheCamilovisk <camilolgon@gmail.com>
2021-02-12 22:42:59 -0300

    Integrating RabbitMQ messages.

--- consumer.py
+++ consumer.py
@@ -1,4 +1,7 @@
 import pika
+import json
+
+from main import Product, db
 
 params = pika.URLParameters(
     "amqps://ohwpsrxf:gl13qpU8lccWdIL0BQJEMN7KoM-8HIjr@eagle.rmq.cloudamqp.com/ohwpsrxf"
@@ -13,7 +16,27 @@ channel.queue_declare(queue="main")
 
 def callback(ch, method, properties, body):
     print("Recieved in main")
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == "product_created":
+        product = Product(id=data["id"], title=data["title"], image=data["image"])
+        db.session.add(product)
+        db.session.commit()
+        print("Product Created")
+
+    elif properties.content_type == "product_updated":
+        product = Product.query.get(data["id"])
+        product.title = data["title"]
+        product.image = data["image"]
+        db.session.commit()
+        print("Product Updated")
+
+    elif properties.content_type == "product_deleted":
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print("Product Deleted")
 
 
 channel.basic_consume(queue="main", on_message_callback=callback, auto_ack=True)
